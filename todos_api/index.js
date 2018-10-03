9d46d8bf4ef7a916a0301886428dd2a2a4bd01ea
ROb <jamesbond00r@gmail.com>
2018-10-03 20:17:47 +0000

    add routes

--- todos_api/index.js
+++ todos_api/index.js
@@ -1,17 +1,23 @@
-// todos_api/index.js - server app
 var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
     bodyParser = require('body-parser');
-var todoRoutes = require('../routes/todos')
     
+var todoRoutes = require("./routes/todos");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname +'/public'));
+app.use(express.static(__dirname + '/views'));
+
 app.get('/', function(req, res){
-   res.send("Hello from the root route"); 
+    res.sendFile("index.html");
 });
 
 app.use('/api/todos', todoRoutes);
 
-
-app.listen(process.env.PORT, function(){
-    console.log("app is running on PORT " + process.env.PORT);
+app.listen(port, function(){
+    console.log("APP IS RUNNING ON PORT " + process.env.PORT);
 })
+    
+    
